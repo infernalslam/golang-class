eabab74c1e5ab6f9b489164147143f0c6790063a
infernalslam <infernal-slam@hotmail.com>
2018-07-10 01:02:11 +0700

    success code

--- main.go
+++ main.go
@@ -5,31 +5,34 @@ import(
 )
 
 func main() {
-	// prime := isPrime(9)
-	// fmt.Printf("%t\n", prime)
-	numbers := []int {2, 4, 5}
-	calculate(numbers)
-	// fmt.Printf("%v\n", number)
-	// send value array in function calculate
+	numbers := []int {1, 2, 3}
+	result, average := calculate(numbers)
+	fmt.Printf("sum = %d, avg = %f\n", result, average)
 
 }
 
 
-func calculate(numbers []int) {
-	// fmt.Printf("%v\n", number)
-	// something check for range
+func calculate(numbers []int) (int, float32){
+	result := 0
+	count := 0
 	for _, number := range numbers {
-		fmt.Printf("%v\n", number)
+		if isPrime(number) {
+			result += number
+			count++
+		}
 	}
+	return result, float32(result / count)
 }
+
 
 func isPrime(num int) bool {
-	numberIsPrime := true
-	for i := 2; i <= num / 2; i++ {
+	prime := num != 1
+	for i := 2; i < num; i++ {
 		if num % i == 0 {
-			numberIsPrime = false
+			prime = false
 			break
 		}
 	}
-	return numberIsPrime
+	return prime
 }
+
