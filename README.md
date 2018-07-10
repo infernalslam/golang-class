11906637156b5d9673377ef86fc926ae0ba8cc23
infernalslam <infernal-slam@hotmail.com>
2018-07-10 19:29:00 +0700

    init days2

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # golang
 
 การประกาศตัวแปร
+
+
+```go
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	pic := make([][]uint8, dy)
+    for i := range(pic) {
+        pic[i] = make([]uint8, dx)
+    }
+	for i := range(pic) {
+        for j := range(pic[i]) {
+            pic[i][j] = uint8 ((i + j) / 1)
+        }
+    }
+    return pic
+}
+
+func main() {
+	pic.Show(Pic)
+}
+
+```
--- main.go
+++ main.go
@@ -1,18 +1,29 @@
 package main
 
-import(
+import (
 	"fmt"
 )
 
 func main() {
-	numbers := []int {1, 2, 3}
+	numbers := []int{1, 2, 3}
 	result, average := calculate(numbers)
 	fmt.Printf("sum = %d, avg = %f\n", result, average)
+	// day2
+	var slice = []string{"a", "b", "c", "d"}
+	copySlice := append([]string(nil), slice...)
+	fmt.Println(copySlice)
+	slice = append(slice, "b", "o", "y")
+	fmt.Println(slice)
 
+	sliceNumber := []int{1, 2, 4, 5, 7, 8}
+	reverseNumber := reverseSlice(sliceNumber)
+	fmt.Println(reverseNumber)
+	// fmt.Println(slice[:2])
+	// fmt.Println(slice[1:2])
+	// fmt.Println(slice[4:])
 }
 
-
-func calculate(numbers []int) (int, float32){
+func calculate(numbers []int) (int, float32) {
 	result := 0
 	count := 0
 	for _, number := range numbers {
@@ -24,11 +35,10 @@ func calculate(numbers []int) (int, float32){
 	return result, float32(result) / float32(count)
 }
 
-
 func isPrime(num int) bool {
 	prime := num != 1
 	for i := 2; i < num; i++ {
-		if num % i == 0 {
+		if num%i == 0 {
 			prime = false
 			break
 		}
@@ -36,3 +46,11 @@ func isPrime(num int) bool {
 	return prime
 }
 
+func reverseSlice(nums []int) []int {
+	for i, j := 0, len(nums)-1; i < j; i, j = i+1, j-1 {
+		tmp := nums[i]
+		nums[i] = nums[j]
+		nums[j] = tmp
+	}
+	return nums
+}
