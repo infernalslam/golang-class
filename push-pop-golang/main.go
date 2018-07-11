15297e6ac49ca5d0cf41fb04c92967e813c016c9
infernalslam <infernal-slam@hotmail.com>
2018-07-11 22:03:02 +0700

    success:fire:

--- push-pop-golang/main.go
+++ push-pop-golang/main.go
@@ -7,8 +7,15 @@ type stack []int
 func main() {
 	var value stack
 	// value variable send to function push()
+	value.push(1)
+	value.push(2)
+	value.push(3)
+	value.push(4)
 	value.push(5)
-	value.push(6)
+
+	// value send to pop() function
+	value.pop()
+	value.pop()
 	fmt.Print(value)
 }
 
@@ -18,6 +25,8 @@ func (s *stack) push(number int) stack {
 	return *s
 }
 
-func (s stack) pop() (int, error) {
+func (s *stack) pop() (int, error) {
+	// fmt.Print(len(*s)) // Get length of a pointer array in golang
+	*s = (*s)[:len(*s)-1]
 	return 0, nil
 }
