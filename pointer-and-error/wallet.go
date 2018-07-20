45c7de996781649e763abd0e17d168e378949b87
infernalslam <infernal-slam@hotmail.com>
2018-07-20 18:48:33 +0700

    run test

--- pointer-and-error/wallet.go
+++ pointer-and-error/wallet.go
@@ -2,14 +2,21 @@ package main
 
 import "fmt"
 
+type Stringer interface {
+	String() string
+}
 type Bitcoin int
 
 type Wallet struct {
 	balance Bitcoin
 }
 
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
 func (w *Wallet) Deposit(amount Bitcoin) {
-	fmt.Printf("address of balance in test is %v", &w.balance)
+	// fmt.Printf("address of balance in test is %v", &w.balance)
 	w.balance += amount
 }
 
--- pointer-and-error/wallet_test.go
+++ pointer-and-error/wallet_test.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
 
-	fmt.Printf("address of balance in test is %v", &wallet.balance)
+		// fmt.Printf("address of balance in test is %v", &wallet.balance)
 
-	got := wallet.Balance()
-	want := Bitcoin(10)
+		got := wallet.Balance()
+		want := Bitcoin(10)
 
-	if got != want {
-		t.Errorf("got %d want %d,", got, want)
-	}
+		if got != want {
+			t.Errorf("got %d want %d,", got, want)
+		}
+	})
 }
