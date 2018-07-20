ed6fe3a259d2f7285df8f9817772283d0048bfed
infernalslam <infernal-slam@hotmail.com>
2018-07-21 01:22:19 +0700

    refactor code

--- learn-struct/learnStruct.go
+++ learn-struct/learnStruct.go
@@ -1,30 +1,30 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"math"
-)
+// import (
+// 	"fmt"
+// 	"math"
+// )
 
-func distance(x1, y1, x2, y2 float64) float64 {
-	a := x2 - x1
-	b := y2 - y1
-	return math.Sqrt(a*a + b*b)
-}
+// func distance(x1, y1, x2, y2 float64) float64 {
+// 	a := x2 - x1
+// 	b := y2 - y1
+// 	return math.Sqrt(a*a + b*b)
+// }
 
-func rectangleArea(x1, y1, x2, y2 float64) float64 {
-	l := distance(x1, y1, x2, y2)
-	w := distance(x1, y1, x2, y2)
-	return l * w
-}
+// func rectangleArea(x1, y1, x2, y2 float64) float64 {
+// 	l := distance(x1, y1, x2, y2)
+// 	w := distance(x1, y1, x2, y2)
+// 	return l * w
+// }
 
-func circleArea(r float64) float64 {
-	return math.Pi * r * r
-}
+// func circleArea(r float64) float64 {
+// 	return math.Pi * r * r
+// }
 
-func main() {
-	fmt.Print("Hello Gopher!")
-	res := distance(1, 2, 3, 4)
-	res = rectangleArea(1, 2, 3, 4)
-	res = circleArea(2.1)
-	fmt.Printf("result %f", res)
-}
+// func main() {
+// 	fmt.Print("Hello Gopher!")
+// 	res := distance(1, 2, 3, 4)
+// 	res = rectangleArea(1, 2, 3, 4)
+// 	res = circleArea(2.1)
+// 	fmt.Printf("result %f", res)
+// }
