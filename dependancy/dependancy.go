3baffa8018307f62a5279f1268011ae383d8f975
infernalslam <infernal-slam@hotmail.com>
2018-07-20 20:07:50 +0700

    fix code

--- dependancy/dependancy.go
+++ dependancy/dependancy.go
@@ -3,9 +3,19 @@ package main
 import (
 	"fmt"
 	"io"
+	"net/http"
 )
 
 // Greet function
 func Greet(w io.Writer, name string) {
 	fmt.Fprintf(w, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	// Greet(os.Stdout, "Elodie")
+	http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
 }
