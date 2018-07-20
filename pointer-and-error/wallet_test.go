ac483efa2e05671b3cac39c71886f6c8e0c98d60
infernalslam <infernal-slam@hotmail.com>
2018-07-20 19:22:45 +0700

    fix code

--- pointer-and-error/wallet_test.go
+++ pointer-and-error/wallet_test.go
@@ -4,28 +4,25 @@ import (
 	"testing"
 )
 
+func assertBalance(t *testing.T, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertError(t *testing.T, got error, want string) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("didn't gott an error but wanted one")
+	}
+	if got.Error() != want {
+		t.Errorf("got '%s', want '%s", got, want)
+	}
+}
+
 func TestWallet(t *testing.T) {
-
-	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
-		t.Helper()
-		got := wallet.Balance()
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	}
-
-	assertError := func(t *testing.T, got error, want string) {
-		t.Helper()
-		if got == nil {
-			t.Fatal("didn't gott an error but wanted one")
-		}
-		if got.Error() != want {
-			t.Errorf("got '%s', want '%s", got, want)
-		}
-		// if err != nil {
-		// 	t.Error("wanted an error but didn't get one")
-		// }
-	}
 
 	t.Run("Withdraw insufficient funds", func(t *testing.T) {
 		startingBalance := Bitcoin(20)
