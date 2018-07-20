9db7a9fb6e19c998e08151704bb35f6de4001724
infernalslam <infernal-slam@hotmail.com>
2018-07-21 02:19:07 +0700

    init test

--- mocking-agian/main.go
+++ mocking-agian/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 const finalWord = "Go!"
@@ -12,8 +13,10 @@ const countdownStart = 3
 // Countdown is function loopback count
 func Countdown(word io.Writer) {
 	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
 		fmt.Fprintln(word, i)
 	}
+	time.Sleep(1 * time.Second)
 	fmt.Fprint(word, finalWord)
 }
 
