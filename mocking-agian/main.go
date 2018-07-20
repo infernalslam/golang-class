6e1417db68e2678cdf40525d9882c72eeb296e73
infernalslam <infernal-slam@hotmail.com>
2018-07-21 02:08:26 +0700

    fix code

--- mocking-agian/main.go
+++ mocking-agian/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+)
+
+// w io.Writer
+
+// Countdown is function loopback count
+func Countdown(number *bytes.Buffer) {
+	fmt.Fprint(number, "3")
+}
 
 func main() {
-	fmt.Println("Hello, Gopher~!☺️")
+	fmt.Println("Hello, Gopher~!☺️ I'm countdown......")
+	// buffer := &bytes.Buffer{}
+	// fmt.Printf("result %b", buffer) // &{[] 0 [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0] 0}%
+	// Countdown()
 }
