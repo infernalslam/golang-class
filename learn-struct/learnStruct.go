ba1b1e74d7ea96c8c6ce2eec418373457d2b9eb5
infernalslam <infernal-slam@hotmail.com>
2018-07-21 01:16:23 +0700

    craet function

---
+++ learn-struct/learnStruct.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func distance(x1, y1, x2, y2 float64) float64 {
+	a := x2 - x1
+	b := y2 - y1
+	return math.Sqrt(a*a + b*b)
+}
+
+func rectangleArea(x1, y1, x2, y2 float64) float64 {
+	l := distance(x1, y1, x2, y2)
+	w := distance(x1, y1, x2, y2)
+	return l * w
+}
+
+func main() {
+	fmt.Print("Hello Gopher!")
+	res := distance(1, 2, 3, 4)
+	fmt.Printf("result %f", res)
+}
