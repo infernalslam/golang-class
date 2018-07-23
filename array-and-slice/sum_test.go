30cf822ca4a101a7e6e5261329ef3c0801c1745b
infernalslam <infernal-slam@hotmail.com>
2018-07-24 00:47:33 +0700

    golang

--- array-and-slice/sum_test.go
+++ array-and-slice/sum_test.go
@@ -4,12 +4,25 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
-	numbers := [5]int{1, 2, 3, 4, 5}
-
-	got := Sum(numbers)
-	want := 15
-
-	if want != got {
-		t.Errorf("got %d want %d griven, %v", got, want, numbers)
+	assertError := func(t *testing.T, got int, want int, numbers []int) {
+		t.Helper()
+		if want != got {
+			t.Errorf("got %d want %d griven, %v", got, want, numbers)
+		}
 	}
+
+	t.Run("sum numbers array 12345;", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
+		assertError(t, got, want, numbers)
+	})
+
+	t.Run("sum numbers array 123;", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
+		assertError(t, got, want, numbers)
+	})
+
 }
